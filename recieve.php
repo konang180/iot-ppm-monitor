02a598dbc398999dca6dfd0dd03636733c2adfa6
konang180 <konangims@gmail.com>
2025-04-01 21:00:14 +0530

    Updated files with latest changes

--- recieve.php
+++ recieve.php
@@ -36,22 +36,14 @@ $location_id = $location['id'];
 
 // Check if record for the hour exists
 $check_query = "SELECT id FROM pollution_data WHERE location_id = $1 AND recorded_date = $2 AND recorded_hour = $3";
-$check_result = pg_query_params($conn, $check_query, [$location_id, $date, $hour]);
 
-if (pg_num_rows($check_result) > 0) {
-    // Update existing record (calculate new average)
-    $update_query = "
-        UPDATE pollution_data 
-        SET average_ppm = (average_ppm + $1) / 2 
-        WHERE location_id = $2 AND recorded_date = $3 AND recorded_hour = $4";
-    pg_query_params($conn, $update_query, [$ppm, $location_id, $date, $hour]);
-} else {
-    // Insert new record
+
+
     $insert_query = "
         INSERT INTO pollution_data (location_id, recorded_date, recorded_hour, average_ppm) 
         VALUES ($1, $2, $3, $4)";
     pg_query_params($conn, $insert_query, [$location_id, $date, $hour, $ppm]);
-}
+
 
 echo "Data received successfully!";
 pg_close($conn);
