1429db7c3ab60ec464a56f2155ac1f7c5aa12aad
konang180 <konangims@gmail.com>
2025-04-30 18:41:45 +0530

    Updated file.php with new changes

--- db_config.php
+++ db_config.php
@@ -1,5 +1,6 @@
+<?php 
 $host = "dpg-d091rrqdbo4c73eeksg0-a.singapore-postgres.render.com";  // Full host with domain
 $port = "5432";
 $dbname = "mydb_xyz123_vz5r";
 $user = "konang";
-$password = "HbpK0zGuFkHURjPM9pK5c5fGZo6pIjOU";
+$password = "HbpK0zGuFkHURjPM9pK5c5fGZo6pIjOU"; ?>
